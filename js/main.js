c1dd253591a7d3f4ab72664208b7a359303b67eb
912vinay <912vinay@gmail.com>
2023-10-12 10:10:30 +0530

    service worker updated

--- js/main.js
+++ js/main.js
@@ -6,4 +6,4 @@ navigator.serviceWorker.register('/sw.js')
 .then(reg=>console.log('Service workder register'))
 .catch(reg=>console.log(`Service Workder failed To register ${reg}`));
     });
-}
+} 
